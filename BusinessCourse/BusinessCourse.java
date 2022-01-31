b42196a4c8ccfa603df3deb60755514fb6015651
DaniEgea <98758188+DaniEgea@users.noreply.github.com>
2022-01-31 19:08:42 +0100

    Update BusinessCourse.java

--- BusinessCourse/BusinessCourse.java
+++ BusinessCourse/BusinessCourse.java
@@ -20,6 +20,47 @@ public class BusinessCourse extends Course{
         this.setDuration(30);
     }
     
+    public void setName(String name){
+        if(name.length()>=4){
+            this.name = name;
+        }
+    }
+    
+    public void setTeacher(String teacher){
+        String nombre = teacher.substring(teacher.indexOf(",")+1);
+        if(teacher.contentEquals(",") && !nombre.contentEquals(" ")){
+            this.teacher = teacher;
+        }
+    }
+    
+    public void setId(String id){
+        if(id.startsWith(name.substring(0, 1)) && id.endsWith(teacher.substring(0, 1))){
+            this.id = id;
+        }
+    }
+    
+    public void setDuration(int duration){
+        if(duration >= 30){
+            this.duration = duration;
+        }
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getTeacher(){
+        return this.teacher;
+    }
+    
+    public String getId(){
+        return this.id;
+    }
+    
+    public int getDuration(){
+        return this.duration;
+    }
+    
     public String toString(){
         return "Name: "+getName()+" | Teacher: "+getTeacher()+" | ID: "+getId()+" | Duration: "+getDuration()+" with an internship that lasts for at least 15 hours.";
     }
